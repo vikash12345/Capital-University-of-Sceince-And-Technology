ea52c7698cd1680eac3c1e1025f6e7e379355964
Vikash Harjeewan <vikash@backgroundcheck.email>
2017-10-19 17:10:23 +0500

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -4,7 +4,7 @@
 
 require 'scraperwiki.php';
 require 'scraperwiki/simple_html_dom.php';
-for($pageloop = 1 ; $pageloop > 3; $pageloop++)
+for($pageloop = 1 ; $pageloop < 3; $pageloop++)
 {
   echo 'http://www.cust.edu.pk/alumni/index.php?r=alumniInfo/allalumni&AlumniInfo_sort=Ref_No&ajax=alumni-info-grid&page='.$pageloop;
 }
